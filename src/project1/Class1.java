a486277088b3c9aedac6812e4ffa3473b23466ce
munafahad <Muna@192.168.1.9>
2015-08-08 22:41:28 -0400

    Class1 has been updated

--- src/project1/Class1.java
+++ src/project1/Class1.java
@@ -4,7 +4,8 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// Test class
-		System.out.print("Hello world!");
+		System.out.print("Hello world! Hello world!");
+		
 	}
 
 }
